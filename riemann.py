b0ec6a2065a721590d6ced1f9d16b96fee39fd9f
Anupam Thakur <at3644@nyu.edu>
2017-09-06 17:02:04 -0400

    Implement riemann

--- riemann.py
+++ riemann.py
@@ -1,15 +1,23 @@
-# This is a python file! The '#' character indicates that the following line is a comment.
+from __future__ import division
+import numpy as np
+""" 
+    Func->f
+    No_Rect->n
+    lower_bound->l
+    upper_bound->u    
+"""
+def Riemann (f,n,l,u):
+    width=(u-l/n)
+    x_vals=np.arange(l+width,u+width,width)
+    fn_values=f(x_vals)
+    result=np.sum(width*fn_values)
+    return result
 
-# The following is an example for how to define a function in Python
-# def tells the compiler that hello_world is the name of a function
-# this implementation of hello_world takes a string as an argument,
-# which has default value of the empty string. If the user calls 
-# hello_world() without an argument, then the compiler uses ''
-# as the default value of the argument.
-def hello_world(name=''):
-    print "hello world!"
-    print name
-    return
+def f(x):
+    return x**2
+
+print Riemann(f(4),10,0,1)
+
+
     
-    
-#Implement the Riemann Sum approximation for integrals.
+
